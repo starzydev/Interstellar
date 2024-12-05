270f2ab7c1cbe983c4fe1b07ccca02d783e34cd2
starzydev <caleb10kglockz@outlook.com>
2024-12-04 19:07:34 -0500

    Password - Password encryption for the app.
    Password encryption so the teachers cant find it!

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Caleb: "calebchase2012",  
+      Guest: "password"
   },
 };
 
